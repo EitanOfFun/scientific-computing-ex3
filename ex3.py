dd1eab5b74bc05587e8af71274a9fa6efd2417d2
Eitan Sternlicht <eitanoffun@gmail.com>
2018-05-10 11:28:10 +0300

    moved plot into chev function instead of in main file

--- ex3.py
+++ ex3.py
@@ -2,27 +2,20 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-
 def chev(a, b, n, f):
     # compute xs in segment [-1, 1]
-    xs = [np.cos((2*i + 1) * np.pi/(2*(n + 1))) for i in range(n + 1)]
+    xs = [np.cos((2*i + 1) * np.pi / (2*(n+1))) for i in range(n + 1)]
 
     # project to segment [a, b]
     ts = [((a + b) / 2) + (((b - a) / 2) * xs[i]) for i in range(n + 1)]
 
-    return np.poly1d(np.polyfit(ts, f(ts), n))
+    p =  np.poly1d(np.polyfit(ts, f(ts), n))
+    x = np.arange(0, np.pi, 0.1)
+    plt.plot(x, p(x))
+    plt.show()
+    return p
 
 
-x = np.arange(0, np.pi, 0.1)
-s = chev(0, np.pi, 3, np.cos)
-plt.plot(x, s(x))
-plt.show()
-
-
-
-
-
-
-
+print(chev(0, np.pi, 3, np.cos))
 
 
